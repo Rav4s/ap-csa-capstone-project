598c3f1bf175b6f593ccf5e6fd5c6a5ed59ea042
Rav4s <ravidhruvishshah@gmail.com>
2022-05-11 12:27:33 -0500

    Move scraper to class with variables

--- src/main/java/scraper/Main.java
+++ src/main/java/scraper/Main.java
@@ -1,30 +1,16 @@
 package scraper;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
-import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlDivision;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String... args) throws IOException {
-        WebClient webClient = new WebClient(BrowserVersion.BEST_SUPPORTED);
-        webClient.setCssErrorHandler(new SilentCssErrorHandler());
-        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-        webClient.getOptions().setCssEnabled(true);
-        webClient.getOptions().setRedirectEnabled(false);
-        webClient.getOptions().setAppletEnabled(false);
-        webClient.getOptions().setJavaScriptEnabled(false);
-        webClient.getOptions().setPopupBlockerEnabled(true);
-        webClient.getOptions().setTimeout(10000);
-        HtmlPage page = webClient.getPage("https://www.formula1.com/en/results.html/2022/races/1124/bahrain/race-result.html");
-        List<HtmlDivision> results = page.getByXPath("//div[@class='resultsarchive-col-right']");
-
-        System.out.println(results.get(0).asXml());
-        webClient.close();
+        String link = "https://www.formula1.com/en/results.html/2022/races/1124/bahrain/race-result.html";
+        String year = "2022";
+        String raceName = "bahrain";
+        Scrape.scrapeRaceResults(year, raceName);
+        Scrape.scrapeRaceResults("2021", "france");
+        Scrape.scrapeRaceResults("2012", "monaco");
+        Scrape.scrapeRaceResults("2019", "spain");
     }
 }
---
+++ src/main/java/scraper/Scrape.java
@@ -0,0 +1,60 @@
+package scraper;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlUnorderedList;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLAnchorElement;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Scrape {
+    public static void scrapeRaceResults(String year, String raceName) throws IOException {
+
+        // Set up webClient
+        WebClient webClient = new WebClient(BrowserVersion.BEST_SUPPORTED);
+        webClient.setCssErrorHandler(new SilentCssErrorHandler());
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        webClient.getOptions().setCssEnabled(true);
+        webClient.getOptions().setRedirectEnabled(false);
+        webClient.getOptions().setAppletEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(false);
+        webClient.getOptions().setPopupBlockerEnabled(true);
+        webClient.getOptions().setTimeout(10000);
+
+        // Get list of links for provided year
+        String yearURL = "https://www.formula1.com/en/results.html/" + year + "/races.html";
+        HtmlPage yearPage = webClient.getPage(yearURL);
+        List<HtmlUnorderedList> yearResults = yearPage.getByXPath("//ul[@class='resultsarchive-filter ResultFilterScrollable']");
+        List<HtmlAnchor> links = yearResults.get(2).getByXPath("//a[@class='resultsarchive-filter-item-link FilterTrigger ']");
+
+        // Find link for specific race
+        String finalLink = null;
+        for (HtmlAnchor a : links) {
+            String link = a.getHrefAttribute();
+            if (link.contains(raceName)) {
+                finalLink = link;
+                break;
+            }
+        }
+
+        if (finalLink == null) {
+            System.out.println("Race/year combination ddoesnt exist");
+            webClient.close();
+            return;
+        }
+
+        // Get race results
+        String url = "https://www.formula1.com/" + finalLink;
+        HtmlPage page = webClient.getPage(url);
+        List<HtmlDivision> results = page.getByXPath("//div[@class='resultsarchive-col-right']");
+
+        System.out.println(results.get(0).asXml());
+        webClient.close();
+    }
+}
